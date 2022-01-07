01490298729488d40302002a3291fe0e0177728a
Sunday P. Afolabi <31808402+Lecon-a@users.noreply.github.com>
2022-01-07 22:14:49 +0100

    Add files via upload

---
+++ projects/title.js
@@ -0,0 +1,24 @@
+const titles = document.querySelectorAll(".pl_a");
+var project_title = document.getElementById("project_title");
+var isClickedTitle;
+
+titles.forEach(title => {
+    title.addEventListener("click", (e) => {
+        captureTitle(title.text);
+    });
+});
+
+function captureTitle(title) {
+    isClickedTitle = title;
+    project_title.innerHTML = title;
+    titles.forEach(title => {
+        if (title.text === isClickedTitle) {
+            title.style.color = "#7cfc00";
+            title.style.borderBottom = "2px solid #7cfc00";
+        } else {
+            title.style.color = "rgb(245, 245, 245)";
+            title.style.borderBottom = "none";
+        }
+    });
+}
+
